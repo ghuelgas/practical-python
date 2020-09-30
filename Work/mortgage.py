8a83b16a7fc2139df5a6cdd6dbb851f350ec9505
ghuelgas <68705415+ghuelgas@users.noreply.github.com>
2020-09-30 12:07:46 -0500

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,37 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+print('Total paid', total_paid)
+
+
+# Exercise 1.8
+
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 1
+
+while principal > 0:
+   
+    if month <= 12:
+      principal = principal * (1+rate/12) - payment - 1000 
+     
+    else:
+      principal = principal * (1+rate/12) - payment
+      
+    month = month + 1
+    total_paid = total_paid + payment
+
+print('Total paid', total_paid)
+print('Months', month)
