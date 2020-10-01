5a38e24341c02b3d251f51b10cf74fec549ba644
ghuelgas <68705415+ghuelgas@users.noreply.github.com>
2020-10-01 12:18:29 -0500

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -14,24 +14,67 @@ print('Total paid', total_paid)
 
 
 # Exercise 1.8
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+extra = 1000
 
+while principal > 0:
+    month = month + 1
+    if month <= 12:
+        principal = principal * (1 + rate / 12) - (payment + extra)
+    else:
+        principal = principal * (1 + rate / 12) - payment
+    total_paid = total_paid + payment
+
+
+print('Total paid', total_paid)
+print( 'Month', month)
+
+# Exercise 1.8
 
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-month = 1
+month = 0
+extra = 1000
+extra_payment_start_month = 61
+extra_payment_end_month = 108
 
 while principal > 0:
-   
-    if month <= 12:
-      principal = principal * (1+rate/12) - payment - 1000 
-     
-    else:
-      principal = principal * (1+rate/12) - payment
-      
     month = month + 1
+    if extra_payment_start_month <= month <= extra_payment_end_month:
+        principal = principal * (1 + rate / 12) - (payment + extra)
+    else:
+        principal = principal * (1 + rate / 12) - payment
     total_paid = total_paid + payment
 
+
 print('Total paid', total_paid)
-print('Months', month)
+print( 'Month', month)
+
+# Exercise 1.9
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+extra = 1000
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+
+while principal > 0:
+    month = month + 1
+    if extra_payment_start_month <= month <= extra_payment_end_month:
+        principal = principal * (1 + rate / 12) - (payment + extra)
+    else:
+        principal = principal * (1 + rate / 12) - payment
+    total_paid = total_paid + payment
+    print(month, total_payed, principal)
+    
+print('Total paid', total_paid)
+print( 'Month', month)
